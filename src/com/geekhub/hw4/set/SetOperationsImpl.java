1a1ab1c97d1a32514c18f063a3dd662c62bf38e5
SergeiDev <shemshur90@gmail.com>
2015-10-30 23:27:26 +0200

    add TaskManagerImpl, SetOperationsImp

---
+++ src/com/geekhub/hw4/set/SetOperationsImpl.java
@@ -0,0 +1,7 @@
+package com.geekhub.hw4.set;
+
+/**
+ * Created by sergei on 10/30/15.
+ */
+public class SetOperationsImpl {
+}
---
+++ src/com/geekhub/hw4/taskmanager/TaskManagerImpl.java
@@ -0,0 +1,7 @@
+package com.geekhub.hw4.taskmanager;
+
+/**
+ * Created by sergei on 10/30/15.
+ */
+public class TaskManagerImpl {
+}
