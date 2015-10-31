b8bcc2924d0c2a94c302043f740576efa4f2556f
SergeiDev <shemshur90@gmail.com>
2015-10-31 11:05:40 +0200

    home work was done

--- src/com/geekhub/hw4/set/SetOperationsImpl.java
+++ src/com/geekhub/hw4/set/SetOperationsImpl.java
@@ -1,7 +1,75 @@
 package com.geekhub.hw4.set;
 
+import com.geekhub.hw4.taskmanager.TaskManagerImpl;
+
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by sergei on 10/30/15.
  */
-public class SetOperationsImpl {
+public class SetOperationsImpl implements SetOperations {
+
+
+    @Override
+    public boolean equals(Set a, Set b) {
+        return a.equals(b);
+    }
+
+    @Override
+    public Set union(Set a, Set b) {
+        HashSet result = new HashSet(a);
+        result.addAll(b);
+        return result;
+    }
+
+
+    @Override
+    public Set subtract(Set a, Set b) {
+        Set result = new HashSet<>();
+        for (Object obj : a) {
+            if (!b.contains(obj)) {
+                result.add(obj);
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public Set intersect(Set a, Set b) {
+        Set result = new HashSet<>(a);
+        result.retainAll(b);
+        return result;
+    }
+
+    @Override
+    public Set symmetricSubtract(Set a, Set b) {
+        return union(subtract(a, b), subtract(b, a));
+    }
+
+    public static void main (String[] args) {
+       SetOperations setOperations = new SetOperationsImpl();
+        Set a = new HashSet<>();
+        Set b = new HashSet<>();
+
+        a.add(1);
+        a.add(2);
+        a.add(3);
+        a.add(4);
+
+        b.add(3);
+        b.add(4);
+        b.add(5);
+        b.add(6);
+
+        System.out.println("Set a = " + a);
+        System.out.println("Set b = " + b);
+        System.out.println("Equals = " + setOperations.equals(a, b));
+        System.out.println("Union = " + setOperations.union(a, b));
+        System.out.println("Subtract = " + setOperations.subtract(a, b));
+        System.out.println("Intersect = " + setOperations.intersect(a,b));
+        System.out.println("Symmetric Subtract = " + setOperations.symmetricSubtract(a,b));
+    }
+
+
 }
--- src/com/geekhub/hw4/taskmanager/TaskManagerImpl.java
+++ src/com/geekhub/hw4/taskmanager/TaskManagerImpl.java
@@ -1,7 +1,95 @@
 package com.geekhub.hw4.taskmanager;
 
+import java.util.*;
+
 /**
  * Created by sergei on 10/30/15.
  */
-public class TaskManagerImpl {
+public class TaskManagerImpl implements TaskManager {
+    private Map<Date, Task> tasks = new TreeMap<>();
+
+    @Override
+    public void addTask(Date date, Task task) {
+        tasks.put(date, task);
+    }
+
+    @Override
+    public void removeTask(Date date) {
+        if (tasks.containsKey(date)) {
+            tasks.remove(date);
+        }
+    }
+
+    @Override
+    public Collection<String> getCategories() {
+        Set<String> categories = new HashSet<>();
+        for (Task task : tasks.values()) {
+            categories.add(task.getCategory());
+        }
+        return categories;
+    }
+
+    @Override
+    public Map<String, List<Task>> getTasksByCategories() {
+        Map<String, List<Task>> tasksByCategories = new TreeMap<>();
+
+        for (String category : getCategories()) {
+            tasksByCategories.put(category, getTasksByCategory(category));
+        }
+        return tasksByCategories;
+    }
+
+    @Override
+    public List<Task> getTasksByCategory(String category) {
+        List<Task> tasksByCategory = new ArrayList<>();
+
+        for (Task task : tasks.values()) {
+            if (task.getCategory().equals(category)) {
+                tasksByCategory.add(task);
+            }
+        }
+        return tasksByCategory;
+    }
+
+    @Override
+    public List<Task> getTasksForToday() {
+        List<Task> tasksForToday = new ArrayList<>();
+        for (Map.Entry<Date, Task> taskEntry : tasks.entrySet()) {
+            if (isToday(taskEntry.getKey())) {
+                tasksForToday.add(taskEntry.getValue());
+            }
+        }
+        return tasksForToday;
+    }
+
+    private boolean isToday(Date date) {
+        if (date == null) {
+            return false;
+        }
+        Date today = new Date();
+        if (date.getYear() == today.getYear() &&
+                date.getMonth() == today.getMonth() &&
+                date.getDay() == today.getDay()) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        TaskManager taskManager = new TaskManagerImpl();
+        taskManager.addTask(new Date(100),new Task("eat","eat breakfast"));
+        taskManager.addTask(new Date(33),new Task("eat","eat diner"));
+        taskManager.addTask(new Date(10000),new Task("work","do homework"));
+        taskManager.addTask(new Date(),new Task("relax","go out"));
+
+        System.out.println(taskManager.getCategories());
+        System.out.println(taskManager.getTasksByCategory("eat"));
+        System.out.println(taskManager.getTasksByCategories());
+        System.out.println(taskManager.getTasksForToday());
+
+        taskManager.removeTask(new Date(10000));
+        System.out.println(taskManager.getCategories());
+
+    }
+
 }
