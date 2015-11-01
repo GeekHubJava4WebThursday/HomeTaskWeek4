95f18d853cc2936fbe5b75fc8cc311f7505d2711
Nicholas.Morhun <Nicksbox78@gmail.com>
2015-11-01 12:54:52 +0200

    TaskManager interface implemented

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,16 @@
 import com.geekhub.hw4.set.SetProcessor;
+import com.geekhub.hw4.taskmanager.Task;
+import com.geekhub.hw4.taskmanager.TasksContainer;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
 
+    private static final long MILLIS_IN_DAY = 86400000;
+
     public static void main(String[] args) {
         testSetOperations();
+        System.out.println();
         testTaskManager();
     }
 
@@ -53,9 +57,63 @@ public class Main {
     }
 
     private static void testTaskManager() {
+        Task[] tasks = {
+                new Task("Main", "1"),
+                new Task("Additional", "2"),
+                new Task("Main", "3"),
+                new Task("Undefined", "4"),
+                new Task("Additional", "5"),
+                new Task("Main", "6")
+        };
 
+        TasksContainer tasksContainer = new TasksContainer();
+
+        tasksContainer.addTask(new Date(System.currentTimeMillis() + MILLIS_IN_DAY), new Task("Main", "tomorrow"));
+        tasksContainer.addTask(new Date(System.currentTimeMillis() - MILLIS_IN_DAY), new Task("Out-of-date", "yesterday"));
+
+        for (Task task : tasks) {
+            tasksContainer.addTask(new Date(), task);
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("Categories:");
+        printCollection(tasksContainer.getCategories());
+
+        System.out.println("Tasks by category:");
+        printTasksList(tasksContainer.getTasksByCategory("Main"));
+
+        System.out.println("Tasks by categories:");
+        printListsInMap(tasksContainer.getTasksByCategories());
+
+        System.out.println("Tasks for today:");
+        printCollection(tasksContainer.getTasksForToday());
     }
 
+    private static void printListsInMap(Map<String, List<Task>> tasksByCategories) {
+        for (String category : tasksByCategories.keySet()) {
+            System.out.println("Category: " + category);
+            List<Task> categoriesTask = tasksByCategories.get(category);
+            for (Task task : categoriesTask) {
+                System.out.print(task + ", ");
+            }
+            System.out.println();
+        }
+    }
 
+    private static <E> void printCollection(Collection<E> collection) {
+        for (E element : collection) {
+            System.out.print(element + " ");
+        }
+        System.out.println();
+    }
+
+    private static void printTasksList(List<Task> taskList) {
+        taskList.forEach(System.out::println);
+        System.out.println();
+    }
 
 }
--- src/com/geekhub/hw4/taskmanager/Task.java
+++ src/com/geekhub/hw4/taskmanager/Task.java
@@ -4,5 +4,21 @@ public class Task {
     private String category;
     private String description;
 
-    /*...*/
+    public Task(String category, String description) {
+        this.category = category;
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{ Category: " + category + "; Description: " + description + " }";
+    }
 }
