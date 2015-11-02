207d901012720e64158144f3676d11f97541e502
unknown <e.okunev@CO-01-1CPROG.alliance.local>
2015-11-02 11:24:34 +0200

    SetHandler implementation

---
+++ src/com/geekhub/hw4/set/SetHandler.java
@@ -0,0 +1,47 @@
+package com.geekhub.hw4.set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetHandler<E> implements SetOperations<E> {
+
+    @Override
+    public boolean equals(Set<E> a, Set<E> b) {
+        return a.containsAll(b) && b.containsAll(a);
+    }
+
+    @Override
+    public Set<E> union(Set<E> a, Set<E> b) {
+        Set<E> result = new HashSet<>(a);
+        result.addAll(b);
+        return  result;
+
+    }
+
+    @Override
+    public Set<E> subtract(Set<E> a, Set<E> b) {
+        Set<E> result = new HashSet<>(a);
+        result.removeAll(b);
+        return  result;
+    }
+
+    @Override
+    public Set<E> intersect(Set<E> a, Set<E> b) {
+        Set<E> result = new HashSet<>(a);
+        result.retainAll(b);
+        return  result;
+    }
+
+    @Override
+    public Set<E> symmetricSubtract(Set<E> a, Set<E> b) {
+        Set<E> result = new HashSet<>(a);
+        result.removeAll(b);
+
+        Set<E> c = new HashSet<>(b);
+        c.removeAll(a);
+
+        result.addAll(c);
+
+        return  result;
+    }
+}
